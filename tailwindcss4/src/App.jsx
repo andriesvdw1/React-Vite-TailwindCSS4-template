2e9a5d070258b2d32313dba2bf3717bbb808392a
andriesvdw1 <andriesvdw1@gmail.com>
2025-05-02 16:51:49 +0200

    apply font to heading

--- tailwindcss4/src/App.jsx
+++ tailwindcss4/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
      <div className="App">
         <div className="flex">
-          <h1 className='text-center text-lg text-blue-400 mt-2 ml-2'>Heading styled with tailwind</h1>
+          <h1 className="text-center text-lg text-blue-400 mt-2 ml-2 font-mono font-exstrabold">Heading styled with tailwind</h1>
         </div>
       </div>
   )
