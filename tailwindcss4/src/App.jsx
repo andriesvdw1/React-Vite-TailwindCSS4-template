6db2a2f17a3ef42ae39eb82af045c504515fa869
andriesvdw1 <andriesvdw1@gmail.com>
2025-05-02 16:46:11 +0200

    Style heading and background of body

--- tailwindcss4/src/App.jsx
+++ tailwindcss4/src/App.jsx
@@ -5,9 +5,11 @@ function App() {
   
 
   return (
-    <>
-      
-    </>
+     <div className="App">
+        <div className="flex">
+          <h1 className='text-center text-lg text-blue-400 mt-2 ml-2'>Heading styled with tailwind</h1>
+        </div>
+      </div>
   )
 }
 
